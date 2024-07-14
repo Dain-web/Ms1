a2e60fcecd2ee0e61f9a188fb92958f9f272e618
Dain-web <ajinaji6110@gmail.com>
2024-07-14 20:02:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -115,3 +115,27 @@ if ('serviceWorker' in navigator) {
     });
   });
 }
+
+
+
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+  // Show your custom install button or prompt
+});
+
+// When user clicks install button
+installButton.addEventListener('click', () => {
+  deferredPrompt.prompt();
+  deferredPrompt.userChoice.then((choiceResult) => {
+    if (choiceResult.outcome === 'accepted') {
+      console.log('User accepted the A2HS prompt');
+    } else {
+      console.log('User dismissed the A2HS prompt');
+    }
+    deferredPrompt = null;
+  });
+});
