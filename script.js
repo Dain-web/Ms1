f171f774148c478ef8c3ec3f3b916e92f33f9cbb
Dain-web <ajinaji6110@gmail.com>
2024-07-14 16:26:47 +0530

    Update 3 script.js

--- script.js
+++ script.js
@@ -103,3 +103,15 @@ document.getElementById('updateDateButton').addEventListener('click', function()
     var audio = new Audio('Sound2.mp3');
     audio.play();
 });
+
+
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, error => {
+      console.log('ServiceWorker registration failed: ', error);
+    });
+  });
+}
